73902d00b939f20090c492dc07762c67b3829765
SaijyothiVadavalli <jyothisai015@gmail.com>
2024-11-27 15:15:38 +0530

    Add files via upload
    Added Script

---
+++ script.js
@@ -0,0 +1,20 @@
+function Addfun()
+{
+    let a=document.getElementById("firstbox").value
+let b=document.getElementById("secondbox").value
+a=parseInt(a)
+b=parseInt(b)
+let sum=a+b
+let r=document.getElementById("result")
+r.innerHTML=sum;
+}
+function Subfun()
+{
+    let a=document.getElementById("firstbox").value
+    let b=document.getElementById("secondbox").value
+    a=parseInt(a)
+    b=parseInt(b)
+    let Sub=a-b
+    let r=document.getElementById("result")
+    r.innerHTML=Sub;
+}
